8ad6d8027fc7818c44bec41cd4a21c06d8e4458f
Candice Smith MacKenzie <Mahony8@aol.com>
2022-01-01 15:52:19 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+
+const todoFormId = 'create-task-form';
+//const newTaskDescription = document.getElementById('new-task-description')
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const todoForm = document.getElementById(todoFormId);
+  todoForm.addEventListener('submit', addToDo);
+  });
+
+function addToDo(e){
+  e.preventDefault();
+  const todoText = e.target.description.value;
+  const taskList = document.getElementById('tasks');
+  const newTask = document.createElement('li');
+  newTask.innerText = todoText;
+  const deleteButton = document.createElement('button');
+  deleteButton.addEventListener('click', handleDelete)
+  deleteButton.textContent = 'x';
+  //newTask.textContent = ` ${} `;
+  newTask.append(deleteButton);
+  taskList.append(newTask);
+  e.target.reset();
+  //document.querySelector("#description").appendChild('li');
+}
+
+function handleDelete(e){
+   e.target.parentNode.remove();
+ }
